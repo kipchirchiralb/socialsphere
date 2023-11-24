69e673907ab6bb55e656976de03c8e3b4499e1d8
albert Kipchirchir <albertkipchirchir02@gmail.com>
2023-11-24 13:06:39 +0300

    new post form created

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ app.use(express.static("public")); // set a folder to server static files
 app.get("/", (req, res) => {
   res.render("home");
 });
-
 app.get("/profile", (req, res) => {
   res.render("profile");
 });
-
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
 //404 error
 app.use((req, res) => {
   res.status(404).render("error", {
